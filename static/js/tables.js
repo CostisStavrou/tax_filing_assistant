3548b2b4ee1e3f171d0e5df715db133048d2ef37
CostisStavrou <108928804+CostisStavrou@users.noreply.github.com>
2024-06-13 21:38:01 +0300

    added afm input

--- static/js/tables.js
+++ static/js/tables.js
@@ -1,21 +1,27 @@
 async function fetchTaxData(afm) {
     if (afm.length !== 9 || isNaN(afm)) {
         console.error('AFM must be exactly 9 digits.');
+        document.getElementById('message').innerText = 'AFM must be exactly 9 digits.';
         return;
     }
 
     try {
         const response = await fetch(`http://127.0.0.1:8000/get_tax_submissions?afm=${afm}`);
         if (!response.ok) {
-            throw new Error(`An error occurred: ${response.statusText}`);
+            if (response.status === 404) {
+                document.getElementById('message').innerText = `No person found with AFM: ${afm}`;
+            } else {
+                throw new Error(`An error occurred: ${response.statusText}`);
+            }
+        } else {
+            const data = await response.json();
+            document.getElementById('personTable').innerHTML = generateTable(data.person_info, "person");
+            document.getElementById('taxDetailsTable').innerHTML = generateTable(data.tax_details_info, "tax_details");
+            document.getElementById('message').innerText = '';
         }
-                      
-        const data = await response.json();
-        document.getElementById('personTable').innerHTML = generateTable(data.person_info, "person" );
-        document.getElementById('taxDetailsTable').innerHTML = generateTable(data.tax_details_info, "tax_details");
-
     } catch (error) {
         console.error('Error fetching tax data:', error);
+        document.getElementById('message').innerText = 'Error fetching tax data. Please try again later.';
     }
 }
 
@@ -56,11 +62,10 @@ function generateTable(data, dataType) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const afm = urlParams.get('afm');
-    if (afm) {
+    document.getElementById('fetchDataButton').addEventListener('click', () => {
+        const afm = document.getElementById('afmInput').value;
         fetchTaxData(afm);
-    }
+    });
 });
 
 
